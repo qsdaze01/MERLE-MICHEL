9c39d22890add88e8d586119f6b021c11c8a1bcf
gaspard <gaspard.michel@insa-lyon.fr>
2021-10-08 10:55:27 +0200

    Problème bind

--- PRS/TP1/client.c
+++ PRS/TP1/client.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
 
 int main(){
     
@@ -15,8 +16,24 @@ int main(){
 
     int reuse = 1;
     setsockopt(socketClient, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    printf("Valeur socket Client : %d \n", socketClient);
 
-    printf("%d \n", socketClient);
+    struct sockaddr_in addr_client;
+    memset((char*) &addr_client, 0, sizeof(addr_client));
+
+    addr_client.sin_addr.s_addr = INADDR_ANY;
+    char addr_any[INET_ADDRSTRLEN];
+    printf("Valeur retournée par aton : %d \n", inet_aton("127.0.0.2", &addr_client.sin_addr.s_addr));
+    inet_ntop(AF_INET, &addr_client.sin_addr.s_addr, addr_any, sizeof(addr_any));
+    printf("Adresse Client : %s \n", addr_any);
+
+    addr_client.sin_port = 1234;
+
+    struct sockaddr_in addr_serv;
+    memset((char*) &addr_serv, 0, sizeof(addr_serv));   
+
+    addr_serv.sin_addr.s_addr = INADDR_ANY;
+    connect(socketClient, &addr_serv.sin_addr.s_addr, sizeof(addr_serv.sin_addr.s_addr));
 
     return(0);
 }
--- PRS/TP1/serveur.c
+++ PRS/TP1/serveur.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 int main(){
 
@@ -13,8 +15,37 @@ int main(){
     }
 
     int reuse = 1;
-    setsockopt(socketClient, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
-    printf("%d \n", socketServeur);
+    setsockopt(socketServeur, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    printf("Valeur socket Client : %d \n", socketServeur);
+
+    struct sockaddr_in addr_serv;
+    memset((char*) &addr_serv, 0, sizeof(addr_serv));
+
+    addr_serv.sin_addr.s_addr = INADDR_ANY;
+    char addr_any[INET_ADDRSTRLEN];
+    printf("Valeur retournée par aton : %d \n", inet_aton("0.0.0.0", (struct in_addr *) &addr_serv.sin_addr.s_addr));
+    inet_ntop(AF_INET, &addr_serv.sin_addr.s_addr, addr_any, sizeof(addr_any));
+    printf("Adresse Serveur : %s \n", addr_any);
+
+    addr_serv.sin_port = 1234;
+
+    if(bind(socketServeur, (struct sockaddr *) &addr_serv.sin_addr.s_addr, (socklen_t) sizeof(addr_serv.sin_addr.s_addr)) == -1){
+        printf("bind \n");
+        return(-1);
+    }
+
+    if(listen(socketServeur, 10) == -1){
+        printf("listen \n");
+        return(-1);
+    }
+
+    while(1){
+        int a = accept(socketServeur, (struct sockaddr *) 0, (socklen_t *) 0);
+        printf("%d \n", a);
+        /*if(accept(socketServeur, (struct sockaddr *) 0, (socklen_t *) 0) != -1){
+            printf("a \n");
+        }*/
+    }
 
     return(0);
 }
