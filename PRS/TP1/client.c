7c63fe6f2fbfffe626f932fd8f88ce54b2fc2847
gaspard <gaspard.michel@insa-lyon.fr>
2021-10-08 08:33:05 +0200

    reuse

--- PRS/TP1/client.c
+++ PRS/TP1/client.c
@@ -7,6 +7,16 @@
 int main(){
     
     int socketClient = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if(socketClient == -1){
+        printf("Erreur socket \n");
+        return(-1);
+    }
+
+    int reuse = 1;
+    setsockopt(socketClient, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+
+    printf("%d \n", socketClient);
 
     return(0);
 }
--- PRS/TP1/serveur.c
+++ PRS/TP1/serveur.c
@@ -7,6 +7,14 @@
 int main(){
 
     int socketServeur = socket(AF_INET, SOCK_STREAM, 0);
+    if(socketServeur == -1){
+        printf("Erreur socket \n");
+        return(-1);
+    }
+
+    int reuse = 1;
+    setsockopt(socketClient, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    printf("%d \n", socketServeur);
 
     return(0);
 }
