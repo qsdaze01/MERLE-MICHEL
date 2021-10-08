49204717e7e58363fb1c2178444591d069d6e7c9
gaspard <gaspard.michel@insa-lyon.fr>
2021-10-08 08:16:08 +0200

    Ajout socket

--- PRS/TP1/serveur.c
+++ PRS/TP1/serveur.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 
 int main(){
+
+    int socketServeur = socket(AF_INET, SOCK_STREAM, 0);
 
     return(0);
 }
