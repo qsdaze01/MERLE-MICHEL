85ffb384be9b0c24903d1183d964d58675bc597f
gaspard <gaspard.michel@insa-lyon.fr>
2021-10-29 18:54:29 +0200

    Bon les ACK ça fait moins n'importe quoi mais c'est toujours pas ça mdrr (Bien changer le RCVSIZE pour le client ET le serveur lors des tests)

--- PRS/TP3/client_udp.c
+++ PRS/TP3/client_udp.c
@@ -108,7 +108,10 @@ int main (int argc, char *argv[]) {
     //sleep(10);
     sendto(com_desc, (const char *)ack, RCVSIZE, 0, (const struct sockaddr *) &adresse_com, len);
     while(taille_reception_fichier > 0){
-        *ack = "ACK";
+        ack[0] = 'A';
+        ack[1] = 'C';
+        ack[2] = 'K';
+        ack[3] = ' ';
         printf("préOK \n");
         taille_reception_fichier = recvfrom(com_desc, buffer_reception_fichier, RCVSIZE, MSG_WAITALL, (struct sockaddr *) &adresse_com, &len);
         printf("Ok \n");
