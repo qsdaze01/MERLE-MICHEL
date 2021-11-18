1d6519157333dc1f58dfc8740a9ef3bfa991ab08
gaspard <gaspard.michel@insa-lyon.fr>
2021-11-18 08:20:29 +0100

    Ajout fichier TIP

--- PRS/Projet/serveur.go
+++ PRS/Projet/serveur.go
@@ -15,10 +15,84 @@ func random(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
+func sent(message []byte, clientAddress *net.UDPAddr, socketCommunication net.UDPConn, RCVSIZE int, RTT float64, timeout int, window int) (int, int, float64){
+	begin := time.Now()
+	_, err := socketCommunication.WriteToUDP(message, clientAddress)
+
+	if err != nil {
+		fmt.Println(err)
+		return -1, window, RTT
+	}
+
+	chanAck := make(chan int, 1)
+	messageAck := make([]byte, RCVSIZE)
+
+	go func() {
+		_, _, err = socketCommunication.ReadFromUDP(messageAck)
+		if err != nil {
+			fmt.Println(err)
+			//fmt.Println("a")
+			return
+		}
+		fmt.Println("Normalement plein : " + string(messageAck))//TODO: Ici messageAck contient bien le bon message en cas d'erreur
+		chanAck <- 1
+		return
+	}()
+
+	if RTT != 0 {
+		select {
+		case <-chanAck:
+
+		case <-time.After(time.Duration(RTT)):
+			fmt.Println("Timeout")
+			window = 1
+			timeout = 1
+		}
+	} else {
+		select {
+		case <-chanAck:
+
+		case <-time.After(1*time.Second):
+			fmt.Println("Timeout")
+			window = 1
+			timeout = 1
+		}
+	}
+	end := time.Now()
+
+	difftime := end.Sub(begin)
+
+	if RTT == 0.0 || timeout == 1 {
+		RTT = 4 * float64(difftime)
+		//timeout = 1
+	} else {
+		RTT = RTT - 0.1*(RTT-4*float64(difftime))
+	}
+
+	if err != nil {
+		fmt.Println(err)
+		return -1, window, RTT
+	}
+	fmt.Println("Normalement vide :" + string(messageAck)) //TODO : Mais là messageAck est vide en cas d'erreur
+	//fmt.Println(string(messageAck[0:3])) //TODO : régler le problème avec l'acquittement des messages. Lorsqu'un message est drop, il ne trouve plus le 'ACK' dans le message
+	if string(messageAck[0:3]) != "ACK" {
+		fmt.Println("Pas de ACK reçu")
+		return 1, 1, RTT
+	}
+
+	if timeout == 0 {
+		window++
+		return 0, window, RTT
+	}
+
+	return 1, window, RTT
+}
+
 func communicate(wg *sync.WaitGroup, port string) {
-	RCVSIZE := 32
+	RCVSIZE := 1024
 	RTT := 0.0
 	timeout := 0
+	window := 1
 	defer wg.Done()
 
 	portCommunication := ":" + port
@@ -48,30 +122,21 @@ func communicate(wg *sync.WaitGroup, port string) {
 		return
 	}
 
-	seq := []byte(strconv.Itoa(random(100000, 900000)))
+	seq := []byte("000001")//[]byte(strconv.Itoa(random(100000, 900000)))
 	numSeq, err := strconv.Atoi(string(seq))
+	//seq = append(seq, byte('\u0000'))
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	count := 0
 	for {
-		//fmt.Println(count)
-		count++
-		fileBuffer := make([]byte, RCVSIZE-10)
-		/*for i := range fileBuffer {
-			fileBuffer[i] = byte('\x00') //sert à rien ici
-		}*/
+		fileBuffer := make([]byte, RCVSIZE-6)
 		_, errEof := file.Read(fileBuffer)
 		message := append(seq, fileBuffer...)
 
 		if errEof == io.EOF {
-			//fmt.Println(count)
-			eof := make([]byte, RCVSIZE-10)
-			/*for i := range eof {
-				eof[i] = 0
-			}*/
+			eof := make([]byte, RCVSIZE-6)
 			eof[0] = byte('F')
 			eof[1] = byte('I')
 			eof[2] = byte('N')
@@ -81,79 +146,45 @@ func communicate(wg *sync.WaitGroup, port string) {
 				fmt.Println(err)
 				return
 			}
-			break //On n'acquitte pas le EOF. C'est normal à cause du break
+			break //On n'acquitte pas le FIN. C'est normal à cause du break
 		} else if errEof != nil {
 			fmt.Println(errEof)
 			return
 		}
 
 		//fmt.Println(message)
-		begin := time.Now()
-		_, err := socketCommunication.WriteToUDP(message, clientAddress)
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		chanAck := make(chan int, 1)
-		messageAck := make([]byte, RCVSIZE-10)
-
-		go func() {
-			_, _, err = socketCommunication.ReadFromUDP(messageAck)
-			if err != nil {
-				fmt.Println(err)
-				fmt.Println("a")
+		for {
+			time.Sleep(500*time.Millisecond)
+			previousRTT := 0.0
+			res := 0
+			res, window, previousRTT = sent(message, clientAddress, *socketCommunication, RCVSIZE, RTT, timeout, window)
+			if res == 0{
+				RTT = previousRTT
+				break
+			}else if res == -1{
+				fmt.Println("Error sent")
 				return
 			}
-			fmt.Println(string(messageAck))
-			chanAck <- 1
-			return
-		}()
-
-		if RTT != 0 {
-			select {
-			case <-chanAck:
-
-			case <-time.After(time.Duration(RTT)):
-				fmt.Println("Timeout")
-				return
-			}
-		} else {
-			select {
-			case <-chanAck:
-
-			case <-time.After(1 * time.Second):
-				fmt.Println("Timeout")
-				return
-			}
+			fmt.Println("Renvoi message")
 		}
-
-		end := time.Now()
-
-		difftime := end.Sub(begin)
-
-		if RTT == 0.0 || timeout == 1 {
-			RTT = 4 * float64(difftime)
-			timeout = 0
-		} else {
-			RTT = RTT - 0.1*(RTT-4*float64(difftime))
-		}
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		if string(messageAck[0:3]) != "ACK" {
-			fmt.Println("Pas de ACK reçu")
-			return
-		}
-
+		fmt.Println(numSeq)
 		numSeq++
-		seq = []byte(strconv.Itoa(numSeq))
-	}
+		if numSeq < 10 {
+			seq = []byte("00000" + strconv.Itoa(numSeq))
+		}else if numSeq < 100 {
+			seq = []byte("0000" + strconv.Itoa(numSeq))
+		}else if numSeq < 1000 {
+			seq = []byte("000" + strconv.Itoa(numSeq))
+		}else if numSeq < 10000 {
+			seq = []byte("00" + strconv.Itoa(numSeq))
+		}else if numSeq < 100000 {
+			seq = []byte("0" + strconv.Itoa(numSeq))
+		}else{
+			seq = []byte(strconv.Itoa(numSeq))
+		}
 
+	}
 }
 
 func main() {
