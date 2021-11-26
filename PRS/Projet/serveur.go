2e746837c2306a1fe9e2fcb60fbe56a89fa56dec
gaspard <gaspard.michel@insa-lyon.fr>
2021-11-26 14:18:03 +0100

    Window qui marche pas

--- PRS/Projet/serveur.go
+++ PRS/Projet/serveur.go
@@ -15,82 +15,47 @@ func random(min, max int) int {
 	return rand.Intn(max-min) + min
 }
 
-func receiveACK(socketCommunication net.UDPConn, messageAck []byte, RTT float64) int {
+func receiveACK(socketCommunication net.UDPConn, messageAck []byte, RTT float64) (int, []byte) {
 	if RTT == 0 {
-		err := socketCommunication.SetReadDeadline(time.Now().Add(500 * time.Millisecond))
+		err := socketCommunication.SetReadDeadline(time.Now().Add(40 * time.Millisecond))
 		if err != nil {
 			fmt.Println(err)
-			return -1
+			return -1, nil
 		}
 	} else {
 		err := socketCommunication.SetReadDeadline(time.Now().Add(time.Duration(RTT)))
 		if err != nil {
 			fmt.Println(err)
-			return -1
+			return -1, nil
 		}
 	}
 
-	taille, _, err := socketCommunication.ReadFromUDP(messageAck)
-	fmt.Println("Taille" + string(taille))
+	_, _, err := socketCommunication.ReadFromUDP(messageAck)
+
+	if err != nil {
+		fmt.Println(err)
+		return -1, nil
+	}
+
+	return 0, messageAck
+}
+
+func send(message []byte, clientAddress *net.UDPAddr, socketCommunication net.UDPConn) int {
+
+	_, err := socketCommunication.WriteToUDP(message, clientAddress)
 	if err != nil {
 		fmt.Println(err)
 		return -1
 	}
-
 	return 0
 }
 
-func send(message []byte, clientAddress *net.UDPAddr, socketCommunication net.UDPConn, RCVSIZE int, RTT float64, timeout int, window int) (int, int, float64) {
-	begin := time.Now()
-	_, err := socketCommunication.WriteToUDP(message, clientAddress)
-
-	if err != nil {
-		fmt.Println(err)
-		return -1, window, RTT
-	}
-
-	messageAck := make([]byte, RCVSIZE)
-
-	if receiveACK(socketCommunication, messageAck, RTT) != 0 {
-		window = 1
-		timeout = 1
-	}
-
-	end := time.Now()
-
-	difftime := end.Sub(begin)
-
-	if RTT == 0.0 || timeout == 1 {
-		RTT = 4 * float64(difftime)
-		//timeout = 1
-	} else {
-		RTT = RTT - 0.1*(RTT-4*float64(difftime))
-	}
-
-	if err != nil {
-		fmt.Println(err)
-		return -1, window, RTT
-	}
-	fmt.Println("Normalement vide :" + string(messageAck)) //TODO : Mais là, messageAck est vide en cas d'erreur
-	//fmt.Println(string(messageAck[0:3])) //TODO : régler le problème avec l'acquittement des messages. Lorsqu'un message est drop, il ne trouve plus le 'ACK' dans le message
-	if string(messageAck[0:3]) != "ACK" {
-		fmt.Println("Pas de ACK reçu")
-		return 1, 1, RTT
-	}
-
-	if timeout == 0 {
-		window++
-		return 0, window, RTT
-	}
-
-	return 1, window, RTT
-}
-
 func communicate(wg *sync.WaitGroup, port string) {
+
 	RCVSIZE := 1024
 	RTT := 0.0
-	timeout := 0
 	window := 1
+	messageSent := 0
 	defer wg.Done()
 
 	portCommunication := ":" + port
@@ -107,8 +72,6 @@ func communicate(wg *sync.WaitGroup, port string) {
 		return
 	}
 
-	//defer socketCommunication.Close()
-
 	filenameClient := make([]byte, 1024)
 	lengthFilenameClient, clientAddress, err := socketCommunication.ReadFromUDP(filenameClient)
 	fmt.Println(string(filenameClient[0:lengthFilenameClient]))
@@ -120,52 +83,87 @@ func communicate(wg *sync.WaitGroup, port string) {
 		return
 	}
 
-	seq := []byte("000001") //[]byte(strconv.Itoa(random(100000, 900000)))
+	seq := []byte("000001")
 	numSeq, err := strconv.Atoi(string(seq))
-	//seq = append(seq, byte('\u0000'))
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	for {
-		fileBuffer := make([]byte, RCVSIZE-6)
-		_, errEof := file.Read(fileBuffer)
-		message := append(seq, fileBuffer...)
 
-		if errEof == io.EOF {
-			eof := make([]byte, 3)
-			eof[0] = byte('F')
-			eof[1] = byte('I')
-			eof[2] = byte('N')
-			_, err := socketCommunication.WriteToUDP(eof, clientAddress)
-			if err != nil {
-				fmt.Println(err)
+	var bufferMessage [][]byte
+	startTimer := time.Now()
+
+	if messageSent < window {
+
+	}
+	for {
+		messageAck := make([]byte, RCVSIZE)
+		difftime := 100 * time.Millisecond
+		res := 1
+		begin := time.Now()
+
+		for i := messageSent; i < window; i++ {
+			fileBuffer := make([]byte, RCVSIZE-6)
+			_, errEof := file.Read(fileBuffer)
+			message := append(seq, fileBuffer...)
+
+			if errEof == io.EOF {
+				eof := make([]byte, 3)
+				eof[0] = byte('F')
+				eof[1] = byte('I')
+				eof[2] = byte('N')
+				_, err := socketCommunication.WriteToUDP(eof, clientAddress)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+
+				endTimer := time.Now()
+				diffTimer := endTimer.Sub(startTimer)
+				fmt.Println(diffTimer)
+
+				break //On n'acquitte pas le FIN. C'est normal à cause du break
+			} else if errEof != nil {
+				fmt.Println(errEof)
 				return
 			}
-			break //On n'acquitte pas le FIN. C'est normal à cause du break
-		} else if errEof != nil {
-			fmt.Println(errEof)
-			return
+			begin = time.Now()
+			res = send(message, clientAddress, *socketCommunication)
+			if res < 0 {
+				fmt.Println("Problème d'envoi")
+				return
+			}
+			bufferMessage = append(bufferMessage, message)
+			messageSent++
 		}
-
-		//fmt.Println(message)
 
 		for {
-			previousRTT := 0.0
-			res := 0
-			res, window, previousRTT = send(message, clientAddress, *socketCommunication, RCVSIZE, RTT, timeout, window)
-			if res == 0 {
-				RTT = previousRTT
+			res, messageAck = receiveACK(*socketCommunication, messageAck, RTT)
+			if res != 0 {
+				window = 1
+				RTT = 4 * float64(difftime)
+				res = send(bufferMessage[0], clientAddress, *socketCommunication)
+				if res < 0 {
+					fmt.Println("Problème d'envoi")
+					return
+				}
+			} else {
+				window++
+				messageSent--
+				RTT = RTT - 0.1*(RTT-4*float64(difftime))
+				bufferMessage = append(bufferMessage[:0], bufferMessage[1:]...)
 				break
-			} else if res == -1 {
-				fmt.Println("Error send")
-				return
-			} else if res == 1 {
-				RTT = 0
 			}
-			fmt.Println("Renvoi message")
 		}
+
+		end := time.Now()
+
+		difftime = end.Sub(begin)
+		//fmt.Println(difftime)
+
+		fmt.Println("Renvoi message")
+
 		fmt.Println(numSeq)
 		numSeq++
 		if numSeq < 10 {
@@ -251,5 +249,4 @@ func main() {
 			portCommunication++
 		}
 	}
-	wg.Wait()
 }
